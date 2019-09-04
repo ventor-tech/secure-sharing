3d82b86ac10ea6ccefa739848fcac0a95233a420
Alexander Pashuk <alexander.pashuk@xpansa.com>
2019-09-04 14:58:33 +0300

    [FIX] Fix testing without .env file

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class BaseConfiguration(object):
     FLASK_DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    REDIS_URL = os.environ.get('REDIS_URL')
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://localhost:6379/0'
     REDIS_PROVIDER = Redis
 
 
